df1700ebdbb3cdfb8d2295618ebcab67a50bf39b
liangyihuai <1757153156@qq.com>
2019-05-02 21:59:41 +0900

    shakespeare

--- src/rnn/dinosaur_name/models/LSTM_Shakespeare.py
+++ src/rnn/dinosaur_name/models/LSTM_Shakespeare.py
@@ -1,30 +1,34 @@
-# Load Packages
 from __future__ import print_function
+
+
 from keras.callbacks import LambdaCallback
-from keras.models import Model, load_model, Sequential
-from keras.layers import Dense, Activation, Dropout, Input, Masking
+from keras.models import Sequential
+from keras.layers import Dense
 from keras.layers import LSTM
+from keras.optimizers import RMSprop
 from keras.utils.data_utils import get_file
-from keras.preprocessing.sequence import pad_sequences
 import numpy as np
 import random
 import sys
 import io
 
-def build_data(text, Tx = 40, stride = 3):
+
+Tx = 40
+
+def build_data(text, Tx=40, stride=3):
     """
     Create a training set by scanning a window of size Tx over the text corpus, with stride 3.
-    
+
     Arguments:
     text -- string, corpus of Shakespearian poem
     Tx -- sequence length, number of time-steps (or characters) in one training example
     stride -- how much the window shifts itself while scanning
-    
+
     Returns:
     X -- list of training examples
     Y -- list of training labels
     """
-    
+
     X = []
     Y = []
 
@@ -33,26 +37,26 @@ def build_data(text, Tx = 40, stride = 3):
         X.append(text[i: i + Tx])
         Y.append(text[i + Tx])
     ### END CODE HERE ###
-    
+
     print('number of training examples:', len(X))
-    
+
     return X, Y
 
 
-def vectorization(X, Y, n_x, char_indices, Tx = 40):
+def vectorization(X, Y, n_x, char_indices, Tx=40):
     """
     Convert X and Y (lists) into arrays to be given to a recurrent neural network.
-    
+
     Arguments:
-    X -- 
-    Y -- 
+    X --
+    Y --
     Tx -- integer, sequence length
-    
+
     Returns:
     x -- array of shape (m, Tx, len(chars))
     y -- array of shape (m, len(chars))
     """
-    
+
     m = len(X)
     x = np.zeros((m, Tx, n_x), dtype=np.bool)
     y = np.zeros((m, n_x), dtype=np.bool)
@@ -60,8 +64,8 @@ def vectorization(X, Y, n_x, char_indices, Tx = 40):
         for t, char in enumerate(sentence):
             x[i, t, char_indices[char]] = 1
         y[i, char_indices[Y[i]]] = 1
-        
-    return x, y 
+
+    return x, y
 
 
 def sample(preds, temperature=1.0):
@@ -71,66 +75,41 @@ def sample(preds, temperature=1.0):
     exp_preds = np.exp(preds)
     preds = exp_preds / np.sum(exp_preds)
     probas = np.random.multinomial(1, preds, 1)
-    out = np.random.choice(range(len(chars)), p = probas.ravel())
+    out = np.random.choice(range(len(chars)), p=probas.ravel())
     return out
-    #return np.argmax(probas)
-    
-def on_epoch_end(epoch, logs):
+    # return np.argmax(probas)
+
+
+
+def on_epoch_end(epoch, _):
     # Function invoked at end of each epoch. Prints generated text.
-    None
-    #start_index = random.randint(0, len(text) - Tx - 1)
+    print()
+    print('----- Generating text after Epoch: %d' % epoch)
 
-    #generated = ''
-    #sentence = text[start_index: start_index + Tx]
-    #sentence = '0'*Tx
-    #usr_input = input("Write the beginning of your poem, the Shakespearian machine will complete it.")
-    # zero pad the sentence to Tx characters.
-    #sentence = ('{0:0>' + str(Tx) + '}').format(usr_input).lower()
-    #generated += sentence
-#
-    #sys.stdout.write(usr_input)
+    start_index = random.randint(0, len(text) - Tx - 1)
+    for diversity in [0.2, 0.5, 1.0, 1.2]:
+        print('----- diversity:', diversity)
 
-    #for i in range(400):
-"""
-        #x_pred = np.zeros((1, Tx, len(chars)))
+        generated = ''
+        sentence = text[start_index: start_index + Tx]
+        generated += sentence
+        print('----- Generating with seed: "' + sentence + '"')
+        sys.stdout.write(generated)
 
-        for t, char in enumerate(sentence):
-            if char != '0':
+        for i in range(400):
+            x_pred = np.zeros((1, Tx, len(chars)))
+            for t, char in enumerate(sentence):
                 x_pred[0, t, char_indices[char]] = 1.
 
-        preds = model.predict(x_pred, verbose=0)[0]
-        next_index = sample(preds, temperature = 1.0)
-        next_char = indices_char[next_index]
+            preds = model.predict(x_pred, verbose=0)[0]
+            next_index = sample(preds, diversity)
+            next_char = indices_char[next_index]
 
-        generated += next_char
-        sentence = sentence[1:] + next_char
+            sentence = sentence[1:] + next_char
 
-        sys.stdout.write(next_char)
-        sys.stdout.flush()
-        
-        if next_char == '\n':
-            continue
-        
-    # Stop at the end of a line (4 lines)
-    print()
- """
-print("Loading text data...")
-text = io.open('../data/shakespeare.txt', encoding='utf-8').read().lower()
-#print('corpus length:', len(text))
-
-Tx = 40
-chars = sorted(list(set(text)))
-char_indices = dict((c, i) for i, c in enumerate(chars))
-indices_char = dict((i, c) for i, c in enumerate(chars))
-#print('number of unique characters in the corpus:', len(chars))
-
-print("Creating training set...")
-X, Y = build_data(text, Tx, stride = 3)
-
-print("Vectorizing training set...")
-x, y = vectorization(X, Y, n_x = len(chars), char_indices = char_indices) 
-print("Loading model...")
-model = load_model('models/model_shakespeare_kiank_350_epoch.h5')
+            sys.stdout.write(next_char)
+            sys.stdout.flush()
+        print()
 
 
 def generate_output():
@@ -140,9 +119,9 @@ def generate_output():
     usr_input = input("Write the beginning of your poem, the Shakespeare machine will complete it. Your input is: ")
     # zero pad the sentence to Tx characters.
     sentence = ('{0:0>' + str(Tx) + '}').format(usr_input).lower()
-    generated += usr_input 
+    generated += usr_input
 
-    sys.stdout.write("\n\nHere is your poem: \n\n") 
+    sys.stdout.write("\n\nHere is your poem: \n\n")
     sys.stdout.write(usr_input)
     for i in range(400):
 
@@ -164,3 +143,39 @@ def generate_output():
 
         if next_char == '\n':
             continue
+
+
+print("Loading text data...")
+text = io.open('../data/shakespeare.txt', encoding='utf-8').read().lower()
+#print('corpus length:', len(text))
+
+
+chars = sorted(list(set(text)))
+char_indices = dict((c, i) for i, c in enumerate(chars))
+indices_char = dict((i, c) for i, c in enumerate(chars))
+#print('number of unique characters in the corpus:', len(chars))
+
+print("Creating training set...")
+X, Y = build_data(text, Tx, stride = 3)
+
+print("Vectorizing training set...")
+x, y = vectorization(X, Y, n_x = len(chars), char_indices = char_indices)
+# print("Loading model...")
+# model = load_model('models/model_shakespeare_kiank_350_epoch.h5')
+
+
+# build the model: a single LSTM
+print('Build model...')
+model = Sequential()
+model.add(LSTM(128, input_shape=(Tx, len(chars))))
+model.add(Dense(len(chars), activation='softmax'))
+
+optimizer = RMSprop(lr=0.01)
+model.compile(loss='categorical_crossentropy', optimizer=optimizer)
+
+print_callback = LambdaCallback(on_epoch_end=on_epoch_end)
+
+model.fit(x, y, batch_size=128, epochs=60, callbacks=[print_callback])
+
+# Run this cell to try with different inputs without having to re-train the model
+generate_output()
